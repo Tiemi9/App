d6a3de85313850ddf94e519d5e44443586afef3f
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-09-14 16:52:56 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let meta = {
+    value: "Ler um livro por mês",
+    checked: false,
+}
+
+let metas = [
+    meta,
+    
+    {
+        value: "Correr 20 minutos por dia",
+        checked: false,
+    }
+]
+
+console.log(metas[0].value)
+console.log(metas[1].value)
