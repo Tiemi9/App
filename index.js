62a03c5d79b33a5896cf9264804705327a442213
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-09-14 18:38:52 -0300

    cadastrar metas - if/else

--- index.js
+++ index.js
@@ -28,7 +28,30 @@ const start = () => {
 start()*/
 
 
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: "Ler um livro por mês",
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+
+    metas.push(
+        {
+            value: meta,
+            checked: false
+        }
+    )
+}
 
 const start = async() => {
 
@@ -59,12 +82,14 @@ const start = async() => {
 
         switch(opcao) {
             case "cadastrar":
-                console.log("Vamos cadastrar?")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("Vamos listar?")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return
 
         }
