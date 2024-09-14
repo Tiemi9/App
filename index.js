092596fcbff9a7911b165b82209e6569e6ee80c1
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-09-14 17:54:48 -0300

    await-async

--- index.js
+++ index.js
@@ -27,10 +27,36 @@ const start = () => {
 
 start()*/
 
-const start = () => {
+
+const { select } = require('@inquirer/prompts')
+
+const start = async() => {
 
     while(true) {
-        let opcao = "sair"
+
+        const opcao = await select({
+
+            message: "menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+
+                },
+                {
+                    name: "Listar meta",
+                    value: "listar"
+
+                },
+                {
+
+                    name: "Sair",
+                    value: "sair"
+
+                }
+            ]
+        })
+
         switch(opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar?")
