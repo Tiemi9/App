0f103bae8b24837e787544d2732759a5b23d75e7
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-09-14 21:24:01 -0300

    metas abertas

--- index.js
+++ index.js
@@ -95,8 +95,25 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "metas realizadas",
+        message: "metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true //pode ser : return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -124,6 +141,11 @@ const start = async() => {
 
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+
+                },
+                {
 
                     name: "Sair",
                     value: "sair"
@@ -142,6 +164,9 @@ const start = async() => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
